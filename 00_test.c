08324901b2c1fc49cb7dd36e19a70fab0258d65a
Fabiola Villanueva <22103326@usc.edu.ph>
2024-09-03 23:24:01 +0800

    Add personalized greeting feature

--- 00_test.c
+++ 00_test.c
@@ -3,6 +3,13 @@
 
 void main () {
     system("cls");
-    printf("\nHello, Kuting! From Bobby.\n\n");
-    printf("new change 1\n\n");
+    printf("\nHello, Kuting! From Bobby.\n");
+    printf("new change 1\n");
+
+    char name[20];
+    printf("Input name: ");
+    scanf("%s", name);
+    printf("Hello, %s!", name);
+
+    printf("\n\n");
 }
